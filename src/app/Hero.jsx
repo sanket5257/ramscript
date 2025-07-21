ccf4b51aaba5bb39523c45aa147b844e73e8389e
Pruthviraj Chavan <chavanpruthviraj9595@gmail.com>
2025-07-21 11:19:26 +0530

    Adding name

--- src/app/Hero.jsx
+++ src/app/Hero.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-      <h1>hello sanket</h1>
+      <h1>hello sanket</h1> 
+      <h1>hello PruthviraJ </h1>
+
     </div>
   )
 }
